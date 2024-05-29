15d84cb1e2a8824d3414fe333390f93eb3e8062a
ropehapi <ropehapi@gmail.com>
2024-05-29 00:34:47 -0300

    GraphQL done

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/ropehapi/clean-architecture-go-expert/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
